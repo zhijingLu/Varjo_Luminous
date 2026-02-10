92ab49680e3252956f8f8337d2aa2d28164763fe
zhijingLu <105914628+zhijingLu@users.noreply.github.com>
2026-02-10 12:32:30 +0100

    Add files via upload

--- StreamDLL/OpenCVdll.bat
+++ StreamDLL/OpenCVdll.bat
@@ -3,22 +3,24 @@ setlocal EnableExtensions EnableDelayedExpansion
 
 REM ==========SETTING=============
 
-set "OPENCV_EDIT_DIR=D:\path\to\opencv_edit"   
-set "OUT_DIR=D:\path\to\build\opencv_edit"    
-set "OPENCV_INC=D:\path\to\opencv\build\include"
-set "OPENCV_LIB=D:\path\to\opencv\build\x64\vc17\lib"
-set "OPENCV_BIN=D:\path\to\opencv\build\x64\vc17\bin"
+set "OPENCV_EDIT_DIR=C:path\to\StreamDLL\Opencv"   
+set "OUT_DIR=C:path\to\StreamDLL\build\Opencv"    
+set "OPENCV_INC=C:path\to\install\include"
+set "OPENCV_LIB=C:path\to\install\lib"
 
-set "OPENCV_LINK_LIBS=opencv_world490.lib"
+REM Build OPENCV_LINK_LIBS from all opencv_*.lib in OPENCV_LIB
+set "OPENCV_LINK_LIBS="
+for %%L in ("%OPENCV_LIB%\opencv_*.lib") do (
+  set "OPENCV_LINK_LIBS=!OPENCV_LINK_LIBS! %%~nxL"
+)
 
 
 
 if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
 
-
 REM ======Compile=================
 
-set "CFLAGS=/nologo /std:c++17 /EHsc /MD /O2"
+set "CFLAGS=/nologo /std:c++17 /EHsc /MD /O2 /GL"
 set "LDFLAGS=/nologo /DLL"
 
 if "%DEBUG%"=="1" (
@@ -27,14 +29,15 @@ if "%DEBUG%"=="1" (
 )
 
 cl %CFLAGS% ^
+  /D VARJOSTREAM_EXPORTS ^
   /I "%OPENCV_EDIT_DIR%" ^
   /I "%OPENCV_INC%" ^
   "%OPENCV_EDIT_DIR%\opencv.cpp" ^
-  /Fo"%OUT_DIR%\opencv_edit.obj" ^
-  /Fe"%OUT_DIR%\opencv_edit.dll" ^
-  /Fd"%OUT_DIR%\opencv_edit.pdb" ^
+  /Fo"%OUT_DIR%\opencv.obj" ^
+  /Fe"%OUT_DIR%\opencv.dll" ^
+  /Fd"%OUT_DIR%\opencv.pdb" ^
   /link %LDFLAGS% ^
-  /LIBPATH:"%OPENCV_LIB%" %OPENCV_LINK_LIBS% ^
+  /LIBPATH:"%OPENCV_LIB%" !OPENCV_LINK_LIBS! ^
   /OUT:"%OUT_DIR%\OpenCV.dll" ^
   /IMPLIB:"%OUT_DIR%\OpenCV.lib"
 
--- StreamDLL/build_dll.bat
+++ StreamDLL/build_dll.bat
@@ -2,38 +2,47 @@
 setlocal EnableDelayedExpansion
 
 REM ====== setting======
-set "V02_DIR=C:\Users\zlu\Downloads\Varjo_Luminous-main\Varjo_Luminous-main\StreamDLL\V02"
-
+set "ROOT=C:\Users\zlu\Downloads\Varjo_Luminous-main\Varjo_Luminous-main\StreamDLL"
+set "V02_DIR=%ROOT%\V02"
+set "OPENCV_EDIT_DIR=%ROOT%\Opencv"
 set "VARJO_INC=C:\Users\zlu\Desktop\project\Varjo_Experimental_SDK_for_Custom_Engines_4.12.0\varjo-sdk-experimental\include"
 set "VARJO_INC_EX=C:\Users\zlu\Desktop\project\Varjo_Experimental_SDK_for_Custom_Engines_4.12.0\varjo-sdk-experimental\include_experimental"
 
 set "VARJO_LIB=C:\Users\zlu\Desktop\project\Varjo_Experimental_SDK_for_Custom_Engines_4.12.0\varjo-sdk-experimental\lib"
 set "OPENCV_INC=C:\Users\zlu\Desktop\Talha\install\include"
 set "OPENCV_LIB=C:\Users\zlu\Desktop\Talha\install\lib"
-set "OPENCV_LINK_LIBS=opencv_core480.lib opencv_imgproc480.lib opencv_imgcodecs480.lib opencv_highgui480.lib opencv_calib3d480.lib opencv_features2d480.lib opencv_flann480.lib opencv_aruco480.lib opencv_ccalib480.lib"
+
+set "OPENCV_LINK_LIBS="
+for %%L in ("%OPENCV_LIB%\opencv_*.lib") do (
+  set "OPENCV_LINK_LIBS=!OPENCV_LINK_LIBS! %%~nxL"
+)
 
 
 
 
 
-REM ====== get all v02  cpp ======
-set SRCS=
+REM ====== get all v02  cpp+Opencv ======
+set "SRCS="
 for /r "%V02_DIR%" %%f in (*.cpp) do (
   set SRCS=!SRCS! "%%f"
 )
+for /r "%OPENCV_EDIT_DIR%" %%f in (*.cpp) do (
+  set SRCS=!SRCS! "%%f"
+)
 
-set OUT_DIR=%cd%\build
+set "OUT_DIR=%ROOT%\build\VarjoStream"
 if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
 pushd "%OUT_DIR%"
 
-
 echo [INFO] Sources: !SRCS!
+echo [INFO] OPENCV_LINK_LIBS: !OPENCV_LINK_LIBS!
 
 
 REM ====== compile ======
-cl /nologo /EHsc /MD /O2 /std:c++17 /LD ^
+cl /nologo /EHsc /MD /O2 /std:c++17 /LD /GL ^
   /D VARJOSTREAM_EXPORTS ^
   /I"%V02_DIR%" ^
+  /I"%OPENCV_EDIT_DIR%" ^
   /I"%VARJO_INC%" ^
   /I"%VARJO_INC_EX%" ^
   /I"%OPENCV_INC%" ^
